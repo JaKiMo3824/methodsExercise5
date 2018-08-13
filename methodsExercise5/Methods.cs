ac20c8bd654bb196870813fd75da7e9c0ea5cc8b
JaKiMo3824 <jacob.moullet@gmail.com>
2018-08-13 14:10:29 -0700

    methodsExercise5
    exercises 1-6 from page 116

---
+++ methodsExercise5/Methods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsExercise5
+{
+    class Methods
+    {
+        public static void Method1(int num1, out int result)
+        {
+            result = num1 / 2;
+            
+        }
+        public static void Method1(double num2, out double result2)
+        {
+            result2 = num2 / 2;
+            
+        }
+        
+        
+    }
+}
--- methodsExercise5/Program.cs
+++ methodsExercise5/Program.cs
@@ -10,6 +10,37 @@ namespace methodsExercise5
     {
         static void Main(string[] args)
         {
+            Methods tryMe = new Methods();
+
+            Console.WriteLine("Please enter any number and I will give it back divided by 2");
+            string user1 = Console.ReadLine();
+
+
+            if (Int32.TryParse(user1, out int num1))
+            {
+                Methods.Method1(num1, out int result);
+                Console.WriteLine(result);
+
+            }
+            else if (Double.TryParse(user1, out double num2))
+            {
+                Methods.Method1(num2, out double result2);
+                Console.WriteLine(result2);
+
+
+            }
+            else
+            {
+                Console.WriteLine("You didn't enter a number");
+            }
+
+            Console.ReadLine();
+            
+
+           
+            
+
+            
         }
     }
 }
